e4fff2e908b919a23d9a6c8ff2883ce5d177c610
Paul Finch <finchy70@gmail.com>
2023-08-14 20:33:21 +0100

    Fixed MAterials edit and delete.

--- resources/views/livewire/daysheet/materials.blade.php
+++ resources/views/livewire/daysheet/materials.blade.php
@@ -97,7 +97,7 @@
                                 <label for="formattedGrandTotal" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Total</label>
                                 <div class="sm:col-span-2 sm:mt-0">
                                     <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input wire:model="formattedGrandTotal" type="text" name="total" id="workDate" readonly class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value="">
+                                        <input wire:model="formattedGrandTotal" type="text" name="formattedGrandTotal" id="formattedGrandTotal" readonly class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value="">
                                     </div>
                                 </div>
                             </div>
